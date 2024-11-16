2eca31f312c3990e013b012449980d106940fcc2
Luciano Cunha <lucianogcunha96@gmail.com>
2024-11-16 18:25:52 -0300

    [FIX] Corrigindo retorno dos dados das listas

--- controllers/listasController.js
+++ controllers/listasController.js
@@ -11,10 +11,31 @@ exports.getData = async (req, res) => {
         const tasks = tasksResult[0];
         const lists = listsResult[0];
 
-        
+        // Organizando os dados das listas
+        let resposta = []
+        let tasksByList = []
+
+        tasks.forEach(task => {
+            if(!tasksByList[task.list_id]) {
+                tasksByList[task.list_id] = [];
+            }
+
+            tasksByList[task.list_id].push(task);
+        });
+
+        console.log(tasksByList);
+
+        lists.forEach(list => {
+            resposta.push({
+                id: list.id,
+                title: list.title,
+                color: list.color,
+                tasks: tasksByList[list.id] ?? []
+            });
+        });
 
         // Retorna os dados como JSON, sem metadados
-        res.json({ tasks, lists });
+        res.json(resposta);
     } catch (error) {
         console.error('Erro ao buscar dados:', error);
         res.status(500).json({ error: 'Erro interno do servidor' });
